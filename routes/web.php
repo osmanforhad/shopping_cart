be36072937bb049ae8e6f5e847b39adb831d66f3
osmanforhad <developer.osmanforhad@gmail.com>
2022-06-08 21:55:06 +0600

    setup view and finish the shopping cart

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/', [ProductController::class, 'index']);
 Route::get('cart', [ProductController::class, 'cart'])->name('cart');
 Route::get('add-to-cart/{id}', [ProductController::class, 'addToCart'])->name('add.to.cart');
